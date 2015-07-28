9c01ca57f709b6e661f40c1f2f12f4aee519de03
Taylor Moss <calvintmoss@gmail.com>
2015-07-28 00:47:29 -0600

    Added paralax background, added base planet model, added tri light shading (might need work)

---
+++ Assets/BasicFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicFollow : MonoBehaviour {
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate() {
+		this.transform.position = new Vector3(target.transform.position.x,
+		                                      target.transform.position.y,
+		                                      -10);
+	}
+}
